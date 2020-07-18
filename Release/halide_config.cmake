6be12b5610645113f99f6300f31254cc4c05cea7
Shantanu Agarwal <shantanu.agarwal1993@gmail.com>
2020-07-19 00:47:47 +0530

    Halide Pre built binaries for Raspberry pi 4. LLVM installed using sudo apt get install (v7.0.1)

---
+++ Release/halide_config.cmake
@@ -0,0 +1,3 @@
+# Machine-Generated: Do Not Edit
+set(HALIDE_SYSTEM_LIBS -lz -lrt -ldl -ltinfo -lpthread -lm -lxml2)
+set(HALIDE_RTTI 1)
---
+++ Release/halide_config.make
@@ -0,0 +1,3 @@
+# Machine-Generated: Do Not Edit
+HALIDE_SYSTEM_LIBS=-lz -lrt -ldl -ltinfo -lpthread -lm -lxml2
+HALIDE_RTTI=1
